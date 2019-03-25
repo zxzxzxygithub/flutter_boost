10536c1710c92472fd5d54b51ceebf794ee2ff15
Yacumima <terry.yacumima@gmail.com>
2019-03-25 14:45:48 +0800

    set main activity as a flutter page

--- example/android/app/src/main/java/com/taobao/idlefish/flutterboostexample/MainActivity.java
+++ example/android/app/src/main/java/com/taobao/idlefish/flutterboostexample/MainActivity.java
@@ -2,35 +2,19 @@ package com.taobao.idlefish.flutterboostexample;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.TextView;
 
 import java.lang.ref.WeakReference;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends FlutterPageActivity {
 
     public static WeakReference<MainActivity> sRef;
 
-    private TextView mOpenNative;
-    private TextView mOpenFlutter;
-    private TextView mOpenFlutterFragment;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         sRef = new WeakReference<>(this);
-
-        setContentView(R.layout.native_page);
-
-        mOpenNative = findViewById(R.id.open_native);
-        mOpenFlutter = findViewById(R.id.open_flutter);
-        mOpenFlutterFragment = findViewById(R.id.open_flutter_fragment);
-
-        mOpenNative.setOnClickListener(this);
-        mOpenFlutter.setOnClickListener(this);
-        mOpenFlutterFragment.setOnClickListener(this);
     }
 
     @Override
@@ -38,16 +22,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onDestroy();
         sRef.clear();
         sRef = null;
-    }
-
-    @Override
-    public void onClick(View v) {
-        if (v == mOpenNative) {
-            PageRouter.openPageByUrl(this, PageRouter.NATIVE_PAGE_URL);
-        } else if (v == mOpenFlutter) {
-            PageRouter.openPageByUrl(this, PageRouter.FLUTTER_PAGE_URL);
-        } else if (v == mOpenFlutterFragment) {
-            PageRouter.openPageByUrl(this, PageRouter.FLUTTER_FRAGMENT_PAGE_URL);
-        }
     }
 }
