04a59ba2267f4f563a1a72a5509b65f5d8d3eb9c
zhengyongxiang <924106037@qq.com>
2019-04-22 11:07:41 +0800

    文件夹调整

---
+++ flutter_boost_example/android/mylibrary/src/main/java/com/zyx/mylibrary/TestUtil.java
@@ -0,0 +1,15 @@
+package com.zyx.mylibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * @author zhengyx
+ * @des
+ * @date 2019/4/10
+ **/
+public class TestUtil {
+    public static void test(Context context) {
+        Toast.makeText(context, "xxxxx", Toast.LENGTH_LONG).show();
+    }
+}
